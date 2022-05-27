3bf1f13fbae081ffbd6b9497dc8e1ece3fda06e7
MaskerDog <37284851+MaskerDog@users.noreply.github>
2022-05-27 09:38:29 +0900

    Initial Commit

---
+++ GitHubAPIClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubAPIClient
+//
+//  Created by npc on 2022/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubAPIClient/GitHubAPIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAPIClientApp.swift
+//  GitHubAPIClient
+//
+//  Created by npc on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAPIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
